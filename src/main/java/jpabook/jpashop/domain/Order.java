525e6c3ba3dc65caf7dcdd0af8d72f01cd11297b
jyung17 <iotaxixi@gmail.com>
2022-12-12 23:21:07 +0900

    Entity 클래스 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,10 +1,13 @@
 package jpabook.jpashop.domain;
 
+import java.awt.Taskbar.Feature;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -23,18 +26,34 @@ public class Order {
   @Column(name = "order_id")
   private Long id;
   
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "member_id")
   private Member member; //주문 회원
 
-  @OneToMany(mappedBy = "order")
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
   private List<OrderItem> orderItems = new ArrayList<>();
   
-  @OneToOne
+  @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
   @JoinColumn(name = "delivery_id")
   private Delivery delivery; //배송정보
   
   private LocalDateTime orderDate; //주문시간
   
   private OrderStatus status; //주문상태 [ORDER, CANCEL]
+  
+  //==연관관계 메서드==//
+  public void setMember(Member member) {
+    this.member = member;
+    member.getOrders().add(this);
+  }
+  
+  public void addOrderItem(OrderItem orderItem) {
+    orderItems.add(orderItem);
+    orderItem.setOrder(this);
+  }
+  
+  public void setDelivery(Delivery delivery) {
+    this.delivery = delivery;
+    delivery.setOrder(this);
+  }
 }
